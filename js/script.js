051a9a32393e3d57e1e02422ab836c29b3b44698
r-alo <email@example.com>
2022-04-22 17:38:30 -0500

    removed past, present, future classes on css; added background color through conditional w/ jQuery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
 // Display today's day and date
 let todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+
+    function hourTracker () {
+        //Constant w/ the current hour
+        const currentHour = moment().hour();
+
+        //check schedule blocks time to set background color
+        $(".scheduleBlock").each(function () {
+
+            let blockTime = parseInt($(this).attr("id"));
+
+            // Conditional to check time and add background color to the schedule Block
+            if (blockTime < currentHour) {
+                $(this).css("background-color", "#d3d3d3");
+            }
+            else if (blockTime === currentHour) {
+                $(this).css("background-color", "#619bff");
+            }
+            else {
+                $(this).css("background-color", "#77dd77");
+            }
+        })
+    };
+
+    hourTracker();
+
+})
